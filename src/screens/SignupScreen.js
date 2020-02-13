742306b22dffaa0a3138100b24fb46d9e7a75e4d
Kiryl <da1z@mail.ru>
2020-02-13 13:02:08 -0800

    initial layout for signup screen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { Text, View, Button, StyleSheet } from "react-native";
-
+import { View, StyleSheet } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+import Spacer from "../components/Spacer";
 const SignupScreen = ({ navigation }) => (
-  <View>
-    <Text>SignupScreen</Text>
-    <Button
-      title="Go To Signin"
-      onPress={() => navigation.navigate("Signin")}
-    ></Button>
-  </View>
+  <>
+    <Spacer>
+      <Text h3>Sign Up for TrackCh</Text>
+    </Spacer>
+    <Input label="Email" />
+    <Spacer />
+    <Input label="Password" />
+    <Spacer>
+      <Button title="Sign Up" />
+    </Spacer>
+  </>
 );
 
+const styles = StyleSheet.create({});
 export default SignupScreen;
