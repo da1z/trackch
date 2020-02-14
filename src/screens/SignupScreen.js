de681955c983d6d87470338b20a93f648b70253f
Kiryl <da1z@mail.ru>
2020-02-13 23:41:57 -0800

    added auth context

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,20 +1,49 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
 import { Text, Input, Button } from "react-native-elements";
 import Spacer from "../components/Spacer";
-const SignupScreen = ({ navigation }) => (
-  <>
-    <Spacer>
-      <Text h3>Sign Up for TrackCh</Text>
-    </Spacer>
-    <Input label="Email" />
-    <Spacer />
-    <Input label="Password" />
-    <Spacer>
-      <Button title="Sign Up" />
-    </Spacer>
-  </>
-);
 
-const styles = StyleSheet.create({});
+const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>Sign Up for TrackCh</Text>
+      </Spacer>
+      <Input
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <Spacer />
+      <Input
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry
+      />
+      <Spacer>
+        <Button title="Sign Up" />
+      </Spacer>
+    </View>
+  );
+};
+SignupScreen.navigationOptions = () => {
+  return {
+    headerShown: false
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 250
+  }
+});
 export default SignupScreen;
