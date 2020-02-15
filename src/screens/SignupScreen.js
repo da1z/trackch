92c2301689560ab33f9fb3ada7247bf15980c7db
Kiryl <da1z@mail.ru>
2020-02-14 23:38:11 -0800

    added sign up functionality

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,15 +1,20 @@
-import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
-import { Text, Input, Button } from "react-native-elements";
-import Spacer from "../components/Spacer";
+import React, { useState, useContext } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const SignupScreen = ({ navigation }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const { state, signup } = useContext(AuthContext);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <View style={styles.container}>
       <Spacer>
-        <Text h3>Sign Up for TrackCh</Text>
+        <Text style={{ alignSelf: 'center' }} h3>
+          Sign Up for TrackCh
+        </Text>
       </Spacer>
       <Input
         label="Email"
@@ -27,8 +32,11 @@ const SignupScreen = ({ navigation }) => {
         autoCorrect={false}
         secureTextEntry
       />
+      {state.errorMessage ? (
+        <Text style={styles.errorMessage}>{state.errorMessage}</Text>
+      ) : null}
       <Spacer>
-        <Button title="Sign Up" />
+        <Button title="Sign Up" onPress={() => signup({ email, password })} />
       </Spacer>
     </View>
   );
@@ -42,8 +50,14 @@ SignupScreen.navigationOptions = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: 'center',
     marginBottom: 250
+  },
+  errorMessage: {
+    fontSize: 16,
+    color: 'red',
+    marginLeft: 15,
+    marginTop: 15
   }
 });
 export default SignupScreen;
