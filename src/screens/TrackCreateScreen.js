0284ecd8b59162369965c4119765fdf0071548e9
Kiryl <da1z@mail.ru>
2020-02-15 19:17:49 -0800

    added map placeholder

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,10 +1,16 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements';
+import { SafeAreaView } from 'react-navigation';
+import Map from '../components/Map';
 
-const TrackCreateScreen = ({ params }) => (
-  <View>
-    <Text>TrackCreateScreen</Text>
-  </View>
-);
+const TrackCreateScreen = () => {
+  return (
+    <SafeAreaView forceInset={{ top: 'always' }}>
+      <Text h3>Create a Track</Text>
+      <Map></Map>
+    </SafeAreaView>
+  );
+};
 
 export default TrackCreateScreen;
