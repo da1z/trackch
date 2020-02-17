b4e9a0719288105023653b6a96ef9abbb81b1b74
Kiryl <da1z@mail.ru>
2020-02-16 20:33:35 -0800

    Track saving to db

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,20 @@
-import axios from "axios";
+import axios from 'axios';
+import { AsyncStorage } from 'react-native';
 
-export default axios.create({
-  baseURL: "http://localhost:3000"
+const instance = axios.create({
+  baseURL: 'http://localhost:3000'
 });
+
+instance.interceptors.request.use(
+  async config => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  err => {
+    return Promise.reject(err);
+  }
+);
+export default instance;
