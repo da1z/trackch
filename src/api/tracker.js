6fb99981e4245d943507e84aee0c29fb08f48edb
Kiryl <da1z@mail.ru>
2020-02-17 21:49:19 -0800

    changed server to heroku

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { AsyncStorage } from 'react-native';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://trackch-server.herokuapp.com'
 });
 
 instance.interceptors.request.use(
